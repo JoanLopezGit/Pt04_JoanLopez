804f72fea4be62d0fa0d28d8bbf98c0639ad60d5
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-20 13:16:48 +0200

    Vista form registrar estructura

---
+++ registrar.vista.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Encapçalament -->
+</head>
+<body>
+    <h1>Registre d'Usuari</h1>
+    <form method="POST" action="register.php">
+        <label for="username">Nom d'usuari:</label>
+        <input type="text" id="username" name="username" required><br>
+        <label for="password">Contrasenya:</label>
+        <input type="password" id="password" name="password" required><br>
+        <input type="submit" value="Registrar-se">
+    </form>
+</body>
+</html>
