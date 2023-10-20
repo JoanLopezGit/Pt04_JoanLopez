f648e4eed6605f6895cd3391becfa317308a8201
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-20 13:16:06 +0200

    Definicio estructura internaform registrar

---
+++ registrar.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'index.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Verificar si l'usuari ja existeix
+    $stmt = $conn->prepare("SELECT * FROM usuaris WHERE username = :username");
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        echo "Aquest usuari ja existeix. Torna a intentar-ho.";
+    } else {
+        // Encriptar la contrasenya
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Inserir l'usuari a la base de dades
+        $stmt = $conn->prepare("INSERT INTO usuaris (username, password) VALUES (:username, :password)");
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+        $stmt->execute();
+
+        echo "Registre completat amb èxit.";
+    }
+}
+
+?>
