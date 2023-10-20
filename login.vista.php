c9f59bf4e275a67ba7f843597e08bdc8d89f9618
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-20 13:06:23 +0200

    Introducció botó vista

---
+++ login.vista.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Encapçalament -->
+</head>
+<body>
+    <h1>Iniciar Sessió</h1>
+    <form method="POST" action="login.php">
+        <label for="username">Nom d'usuari:</label>
+        <input type="text" id="username" name="username" required><br>
+        <label for="password">Contrasenya:</label>
+        <input type="password" id="password" name="password" required><br>
+        <input type="submit" value="Iniciar Sessió">
+    </form>
+</body>
+</html>
