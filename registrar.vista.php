57602bd46e8d58dba9b4f3a88453994ecaf38184
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-20 13:20:29 +0200

    Mosificaió confirmar contra

--- registrar.vista.php
+++ registrar.vista.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Encapçalament -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="estils.css">
 </head>
 <body>
-    <h1>Registre d'Usuari</h1>
-    <form method="POST" action="register.php">
-        <label for="username">Nom d'usuari:</label>
-        <input type="text" id="username" name="username" required><br>
-        <label for="password">Contrasenya:</label>
-        <input type="password" id="password" name="password" required><br>
-        <input type="submit" value="Registrar-se">
-    </form>
+    <div class="contenidor">
+        <h1>Registro de Usuario</h1>
+        <form method="POST" action="register.php">
+            <input type="text" name="username" placeholder="Nombre de Usuario" required><br>
+            <input type="password" name="password" placeholder="Contraseña" required><br>
+            <input type="password" name="confirm_password" placeholder="Confirmar Contraseña" required><br>
+            <input type="submit" value="Registrar">
+        </form>
+    </div>
 </body>
 </html>
