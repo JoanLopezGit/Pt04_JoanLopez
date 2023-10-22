fd8ace3659c834106b898b392ed012a01615963a
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-22 21:59:32 +0200

    session documentation

--- Joan_López_Pt04/Controlador/session.php
+++ Joan_López_Pt04/Controlador/session.php
@@ -1,19 +1,31 @@
 <?php
 
+/**
+ * Inicia una nova sessió.
+ *
+ * @param string $usuari L'usuari de la sessió.
+ * @param string $nom El nom associat a l'usuari.
+ */
 function startSession($usuari,$nom){
     session_start();
     $_SESSION["newsession"]=$usuari;
     $_SESSION["nom"]=$nom;
-    header("Location: ../Vista/index.vista.php");
-                exit();
-
+    header("Location: ../Vista/index.vista.php"); // Redirigeix a la pàgina d'inici
+    exit();
 }
+
+/**
+ * Neteja les dades de possibles caràcters especials.
+ *
+ * @param string $data Les dades a netejar.
+ *
+ * @return string Les dades netejades.
+ */
 function netejaDades($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    $data = trim($data); // Elimina espais en blanc al principi i final
+    $data = stripslashes($data); // Elimina les barres invertides
+    $data = htmlspecialchars($data); // Converteix caràcters especials en entitats HTML
+    return $data; // Retorna les dades netejades
 }
-
 
 ?>
