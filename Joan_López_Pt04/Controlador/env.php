ba64d6940389217a9be5eb39559ca600aa9b39ff
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-22 22:01:15 +0200

    phpdoc constants

--- Joan_López_Pt04/Controlador/env.php
+++ Joan_López_Pt04/Controlador/env.php
@@ -1,6 +1,7 @@
 <?php 
-define("DB_HOST", "localhost");
-define("DB_NAME","pt04_joan_lopez");
-define("DB_USER","root");
-define("DB_PASSWORD","");
+// Definició de les constants per a la connexió a la base de dades
+define("DB_HOST", "localhost");  // Adreça del servidor de la base de dades
+define("DB_NAME","pt04_joan_lopez");  // Nom de la base de dades
+define("DB_USER","root");  // Nom d'usuari de la base de dades
+define("DB_PASSWORD","");  // Contrasenya de la base de dades
 ?> 
