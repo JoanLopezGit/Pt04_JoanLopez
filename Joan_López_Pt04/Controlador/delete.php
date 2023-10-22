dd9e674a5110714c46a8478923b7c7934bdabd03
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-22 21:52:54 +0200

    PHPDocumentor delete

--- Joan_López_Pt04/Controlador/delete.php
+++ Joan_López_Pt04/Controlador/delete.php
@@ -1,7 +1,14 @@
 <?php 
-require_once("../Model/Model.php");
-require_once("../Controlador/session.php");
+require_once("../Model/Model.php"); // Inclou el fitxer Model.php
+require_once("../Controlador/session.php"); // Requereix l'ús de la sessió
 
+/**
+ * Funció per validar l'ID de l'article.
+ *
+ * @param int $idArticle L'ID de l'article a validar.
+ *
+ * @return string Missatge d'error, si hi ha algun.
+ */
 function validarDades($idArticle){
     $errors="";
     if(empty($idArticle)){
@@ -11,25 +18,30 @@ function validarDades($idArticle){
 }
 
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    $idArticle = netejaDades($_POST["id_article"]);
+    $idArticle = netejaDades($_POST["id_article"]); // Neteja les dades de l'ID de l'article
 
+    // Validació de dades
     $errors=validarDades($idArticle,$article=null);
-    $msg_confirm="";
+
+    $msg_confirm=""; 
 
     if($errors==""){
        
-        if(conectarArticleUser($idArticle)){
-            try{deleteArticle($idArticle);
-                $msg_confirm.="<br> Article Esborrat";}
-            catch(PDOException $e){
+        if(conectarArticleUser($idArticle)){ // Intenta connectar l'article amb l'usuari
+            try{
+                deleteArticle($idArticle); // Intenta esborrar l'article
+                $msg_confirm.="<br> Article Esborrat"; // Missatge de confirmació
+
+            } catch(PDOException $e){
+
+                // Error de connexió
                 $errors.="Connection issue";
             }
         }else{
-            $errors.="No es pot accedir al article.";
+            $errors.="No es pot accedir al article."; // Error si no es pot accedir a l'article
         }
     }
 }
 
-require_once("../Vista/delete.vista.php");
-
+require_once("../Vista/delete.vista.php"); // Inclou el fitxer delete.vista.php
 ?>
