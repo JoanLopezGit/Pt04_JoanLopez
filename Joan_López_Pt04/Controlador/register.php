241fd1e3c1a18bf5adb5e2f5c87616ddaff1a7c3
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-10-22 21:57:09 +0200

    PHP docuemntor

--- Joan_López_Pt04/Controlador/register.php
+++ Joan_López_Pt04/Controlador/register.php
@@ -2,6 +2,16 @@
 require_once("../Model/Model.php");
 require_once("../Controlador/session.php");
 
+/**
+ * Valida les dades d'un usuari.
+ *
+ * @param string $nom El nom de l'usuari.
+ * @param string $correu L'adreça electrònica de l'usuari.
+ * @param string $password La contrasenya de l'usuari.
+ * @param string $confirm_password La confirmació de la contrasenya de l'usuari.
+ *
+ * @return string Un missatge d'error si hi ha hagut algun problema amb les dades, o bé una cadena buida si les dades són correctes.
+ */
 function validarDades($nom='',$correu="",$password="",$confirm_password=""){
     $errors="";
      if (!preg_match("/^[a-zA-Z-' ]*$/", $nom)) {
@@ -35,5 +45,6 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
         }    
     }
 }
+
 require_once("../Vista/register.vista.php");
 ?>
